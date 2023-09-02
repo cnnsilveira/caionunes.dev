601c872876d2aba02a9b7391a22e46776c5a3661
Caio Nunes <caio@caionunes.dev>
2023-09-01 23:15:20 -0300

    1.0 files

---
+++ 404.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+        <style>
+            #wrap-404 {
+                width: 100dvw;
+                height: 100dvh;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            }
+            span {
+                font-size: 3rem;
+                color: #fff;
+            }
+        </style>
+        <div id="wrap-404">
+            <span>404 - Not found</span>
+        </div>
+<?php get_footer(); ?>
